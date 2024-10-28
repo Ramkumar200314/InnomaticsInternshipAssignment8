5391b335595f8a00a251782a476e3d8853d14868
Ram Kumar <ramkumar20034@gmail.com>
2024-10-28 17:50:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import RegisterStudent from './pages/RegisterStudent';
+import ListStudents from './pages/ListStudents';
+import StudentDetailsPage from './pages/StudentDetailsPage';
+import Dashboard from './components/Dashboard'; // Make sure this component exists
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/register" element={<RegisterStudent />} />
+          <Route path="/students" element={<ListStudents />} />
+          <Route path="/students/:id" element={<StudentDetailsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
